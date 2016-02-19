5449966bfd890bef39ba81d261aaa24800026822
ilaotan <tanliansheng2011@gmail.com>
2016-02-19 10:55:22 +0800

    当注入的对象类型不是唯一时,需要指定别名 来正确注入需要的对象

--- src/main/java/com/springBootD/framework/config/MyBatisConfig.java
+++ src/main/java/com/springBootD/framework/config/MyBatisConfig.java
@@ -30,6 +30,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -53,6 +54,7 @@ import java.util.Properties;
 public class MyBatisConfig implements TransactionManagementConfigurer {
 
     @Autowired
+    //@Qualifier("123")
     DataSource dataSource;
 
     @Bean(name = "sqlSessionFactory")
