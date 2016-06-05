234ea30dc8281248ecd7742e8486613de4a0694a
ilaotan <tanliansheng2011@gmail.com>
2016-06-06 01:37:45 +0800

    测试使用lombok  减少代码

--- src/test/java/com/springBootD/test/test/FastJsonTest.java
+++ src/test/java/com/springBootD/test/test/FastJsonTest.java
@@ -5,6 +5,7 @@ import com.springBootD.framework.utils.JsonUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /**
  * Created by tan on 2016/6/5.
@@ -16,10 +17,14 @@ public class FastJsonTest {
 
 
         Test test = new Test("1","张三","123456",99);
-
         System.out.println(test);
 
         System.out.println(JsonUtils.toJSONString(test));
+
+        Test test2 = new Test("1","张三","",99);
+
+        System.out.println(test2);
+
 
     }
 
@@ -34,7 +39,7 @@ class Test{
     private String id;
 
     private String userName;
-
+//@NonNull //当字段为null时,会抛异常
     private String password;
 
     @JSONField(serialize = false)
